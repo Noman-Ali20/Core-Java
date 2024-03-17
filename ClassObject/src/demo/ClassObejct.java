bf71005bc95a91cc7c02ab99c5061b9cc4ad1bf6
MrPrince20 <100679592+MrPrince20@users.noreply.github.com>
2024-03-18 00:06:55 +0530

    Class and Object

---
+++ ClassObject/src/demo/ClassObejct.java
@@ -0,0 +1,19 @@
+package demo;
+class Ref{
+void m1() {
+	int x = 10;
+	System.out.println(x);
+}
+}
+
+
+public class ClassObejct {
+
+	public static void main(String[] args) {
+		
+		Ref xyz = new Ref();
+		xyz.m1();
+
+	}
+
+}
